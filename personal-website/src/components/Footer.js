3662e1d0719824d00c0ed3a1f3f460447081d73a
Jonathan Zhou <Jonathanzhou8@gmail.com>
2023-01-04 20:19:52 -0700

    Changed to Mecha Mayhem

--- personal-website/src/components/Footer.js
+++ personal-website/src/components/Footer.js
@@ -1,25 +1,14 @@
 import React from "react";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
 import "../styles/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <div className="socialMedia">
-        <a href="https://www.linkedin.com/in/jonathanzhou21/">
-          <LinkedInIcon />
-        </a>
-        <a href="mailto:jonathan.zhou@uwaterloo.ca">
-          <EmailIcon />
-        </a>
-
-        <a href="https://github.com/JonZ21">
-          <GitHubIcon />
-        </a>
-      </div>
-      <p className="copyright"> &copy; 2022 Jonathan Zhou</p>
+      <div className="socialMedia"></div>
+      <p className="copyright">
+        {" "}
+        &copy; 2023 Western Mechatronics Robotics Club
+      </p>
     </div>
   );
 }
--- personal-website/src/pages/Home.js
+++ personal-website/src/pages/Home.js
@@ -1,43 +1,20 @@
 import React from "react";
-import GitHubIcon from "@mui/icons-material/GitHub";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import EmailIcon from "@mui/icons-material/Email";
 import { Link } from "react-router-dom";
-import ProjectItem from "../components/ProjectItem";
 import "../styles/Home.css";
 import "../styles/Projects.css";
 
 //Images for Current Projects
-import stitcher from "../assets/stitcher.png";
-import tmHack from "../assets/tmhack.png";
-import card from "../assets/card.png";
+import mmlogo from "../assets/mmlogo.png";
 
 function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2>Hey, I'm Jon!</h2>
-        <div className="prompt">
-          <p>Mechatronics Student and Robotics Entrepreneur </p>
-          <a href="https://www.linkedin.com/in/jonathanzhou21/">
-            <LinkedInIcon />
-          </a>
-          <a href="mailto:jonathan.zhou@uwaterloo.ca">
-            <EmailIcon />
-          </a>
-
-          <a href="https://github.com/JonZ21">
-            <GitHubIcon />
-          </a>
-        </div>
+        <img src={mmlogo} alt="logo" className="hero" />
+        <h2>Mecha Mayhem Official App</h2>
+        <div className="prompt"></div>
       </div>
       <div className="currentProjects">
-        <h1>Currently Working On</h1>
-        <div className="projectList">
-          <ProjectItem name="VEX Robotics TM Hack" image={tmHack} id={0} />
-          <ProjectItem name="Family Photo Stitcher" image={stitcher} id={1} />
-          <ProjectItem name="NFC LinkedIN Card" image={card} id={2} />
-        </div>
         <div className="seeMore">
           <Link
             to="/Projects"
